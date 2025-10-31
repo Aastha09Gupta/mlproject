56b3b2bfd30ca7bed9a3f5a176f0cbd79baa8844
Aastha09Gupta <ag3162517@gmail.com>
2025-10-31 16:11:49 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -22,14 +23,21 @@ def save_object(file_path, obj):
         raise CustomException("Error occurred while saving object", sys) from e
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train,y_train) #Train model
+            gs=GridSearchCV(model,para,cv=3,verbose=0)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train,y_train) #Train model
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
 
